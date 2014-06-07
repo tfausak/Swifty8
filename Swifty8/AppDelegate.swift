45314b5673016b6e4f667a0822dcdcf041cfb865
Taylor Fausak <taylor@fausak.me>
2014-06-07 08:51:54 -0500

    Initial Commit

---
+++ Swifty8/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swifty8
+//
+//  Created by Taylor Fausak on 6/7/14.
+//  Copyright (c) 2014 Taylor Fausak. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swifty8/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swifty8
+//
+//  Created by Taylor Fausak on 6/7/14.
+//  Copyright (c) 2014 Taylor Fausak. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
